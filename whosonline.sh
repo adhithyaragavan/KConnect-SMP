555dd058c5ff6794558f5a97a97fbc5b30a35aff
adhithyaragavan <adhithya.ragavan07@gmail.com>
2026-05-25 23:48:12 +0400

    Delete whosonline.sh

--- whosonline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#State → current connection status
-#Local Address → your machine’s IP address and port
-#Remote Address → destination IP address and port
-#Process → program/process using the connection
-ss -tunp | column -t
-
